be35f39dc272e38e49da6684533f4d410572f842
cheerzhang <zhanglenus@gmail.com>
2023-09-04 17:50:56 +0200

    add logging

--- gender_model/server.py
+++ gender_model/server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 from gender_model.config import Config
+import logging
+import sys
 
 app = Flask(__name__)
 model = joblib.load('gender_model/model/logistic_gender.pkl')
@@ -10,13 +12,33 @@ app.config.from_object(Config)
 api_path_prefix = app.config['API_PATH_PREFIX']
 api_model_version = app.config['MODEL_VERSION']
 
+# Create a custom logger
+logger = logging.getLogger(__name__)
+# Define log handlers for STDOUT (INFO, NOTICE, DEBUG) and STDERR (WARNING, ERROR, CRITICAL)
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+stderr_handler = logging.StreamHandler(stream=sys.stderr)
+stderr_handler.setLevel(logging.WARNING)
+# Define log formats
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stdout_handler.setFormatter(log_format)
+stderr_handler.setFormatter(log_format)
+# Add the handlers to the logger based on log levels
+logger.addHandler(stdout_handler)  # INFO, NOTICE, DEBUG will go to STDOUT
+logger.addHandler(stderr_handler)  # WARNING, ERROR, CRITICAL will go to STDERR
+
 @app.route(f'{api_path_prefix}/health', methods=['GET'])
 def check_health():
-    X_pred_vec = vectorizer.transform(['peter'])
-    y_pred = model.predict(X_pred_vec)
-    y_pred = int(y_pred[0])
-    res = {'alive': y_pred}
-    return jsonify(res)
+    try:
+        X_pred_vec = vectorizer.transform(['peter'])
+        y_pred = model.predict(X_pred_vec)
+        y_pred = int(y_pred[0])
+        res = {'alive': y_pred}
+        return jsonify(res)
+    except Exception as e:
+        logger.error(f"Exception occurred: {str(e)}")
+        res = {'alive': 0}
+        return jsonify(res), 500
 
 @app.route(f'{api_path_prefix}/predict_gender_v1', methods=['POST'])
 def predict_gender():
