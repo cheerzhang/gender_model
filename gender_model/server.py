00e617cd829d6ff1a3cc14ee226b5771ac881052
le.zhang <le.zhang@silot.ai>
2023-09-05 20:50:46 +0200

    Update server.py

--- gender_model/server.py
+++ gender_model/server.py
@@ -12,39 +12,17 @@ app.config.from_object(Config)
 api_path_prefix = app.config['API_PATH_PREFIX']
 api_model_version = app.config['MODEL_VERSION']
 
-# Create a custom logger
-logger = logging.getLogger(__name__)
-# ... Rest of your logging configuration ...
-stdout_handler = logging.StreamHandler(stream=sys.stdout)
-stdout_handler.setLevel(logging.INFO)
-stderr_handler = logging.StreamHandler(stream=sys.stderr)
-stderr_handler.setLevel(logging.WARNING)
-# Define log formats
-log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stdout_handler.setFormatter(log_format)
-stderr_handler.setFormatter(log_format)
-# Add the handlers to the logger based on log levels
-logger.addHandler(stdout_handler)  # INFO, NOTICE, DEBUG will go to STDOUT
-logger.addHandler(stderr_handler)  # WARNING, ERROR, CRITICAL will go to STDERR
-
-
-# for gunicorn
-logger_gunicorn = logging.getLogger('gunicorn.info')
-stdout_handler_gunicorn = logging.StreamHandler(stream=sys.stdout)
-stdout_handler_gunicorn.setLevel(logging.INFO)
-logger_gunicorn.addHandler(stdout_handler_gunicorn)
-
 @app.route(f'{api_path_prefix}/health', methods=['GET'])
 def check_health():
     try:
         X_pred_vec = vectorizer.transform(['peter'])
         y_pred = model.predict(X_pred_vec)
         y_pred = int(y_pred[0])
-        logger.info(f'predict of peter is: {y_pred}')
+        app.logger.info(f'predict of peter is: {y_pred}')
         res = {'alive': y_pred}
         return jsonify(res)
     except Exception as e:
-        logger.error(f"Exception occurred: {str(e)}")
+        app.logger.error(f"Exception occurred: {str(e)}")
         res = {'alive': 0}
         return jsonify(res), 500
 
@@ -59,9 +37,10 @@ def predict_gender():
 
     predicted_genders = ['M' if pred == 1 else 'F' for pred in y_pred]
     res = {'Predict of gender': predicted_genders}
-    logger.info(f"predict of {df['first_name'].values} is: {predicted_genders}")
+    app.logger.info(f"predict of {df['first_name'].values} is: {predicted_genders}")
     return jsonify(res)
 
 if __name__ == '__main__':
-    logger.setLevel(logging.INFO)
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
     app.run(host='0.0.0.0', port=5002)
