a1a74bf97d324a052f51b12eb34c3fa047ff63ac
cheerzhang <zhanglenus@gmail.com>
2023-09-08 09:22:00 +0200

    Update server.py

--- gender_model/server.py
+++ gender_model/server.py
@@ -44,6 +44,7 @@ def predict_gender():
         df = pd.DataFrame(json_data)
         X_pred_vec = vectorizer.transform(df['first_name'].values)
         y_pred = model.predict(X_pred_vec)
+        app.logger.debug(y_pred)
         predicted_genders = ['M' if pred == 1 else 'F' for pred in y_pred]
         res = {'Predict of gender': predicted_genders}
         # app.logger.info(f"predict of {df['first_name'].values} is: {predicted_genders}")
