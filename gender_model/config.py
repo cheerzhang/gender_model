eed084e11a89cb28e4cad3faa246c699a62cb597
cheerzhang <zhanglenus@gmail.com>
2023-09-19 10:38:05 +0200

    add doc for api

--- gender_model/config.py
+++ gender_model/config.py
@@ -1,3 +1,36 @@
+import logging, sys
+
 class Config:
     API_PATH_PREFIX = '/api'
     MODEL_VERSION = 'v1'
+
+class LessThanFilter(logging.Filter):
+    def __init__(self, exclusive_maximum, name=""):
+        super(LessThanFilter, self).__init__(name)
+        self.max_level = exclusive_maximum
+
+    def filter(self, record):
+        #non-zero return means we log this message
+        return 1 if record.levelno < self.max_level else 0
+
+class config_Logger:
+    def __init__(self, logger):
+        self.logger = logger
+    def config_level(self):
+        self.logger.setLevel(logging.DEBUG)
+        # Create handlers for stdout (INFO, NOTICE, DEBUG) and stderr (WARNING, ERROR, CRITICAL)
+        stdout_handler = logging.StreamHandler(stream=sys.stdout)
+        stderr_handler = logging.StreamHandler(stream=sys.stderr)
+        # Set the log level for each handler
+        stderr_handler.setLevel(logging.WARNING)  # Log WARNING and above to stderr
+        stdout_handler.setLevel(logging.DEBUG)  # Log INFO and above to stdout
+        stdout_handler.addFilter(LessThanFilter(logging.WARNING))
+        # Define log formats (you can customize these as needed)
+        log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stdout_handler.setFormatter(log_format)
+        stderr_handler.setFormatter(log_format)
+        # Add the handlers to the logger based on log levels
+        self.logger.handlers.clear()
+        self.logger.addHandler(stderr_handler)  # WARNING, ERROR, CRITICAL will go to stderr
+        self.logger.addHandler(stdout_handler)  # INFO, NOTICE, DEBUG will go to stdout
+        return self.logger
--- gender_model/server.py
+++ gender_model/server.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import joblib
 import sys
-from gender_model.config import Config
-import logging
+from gender_model.config import Config, config_Logger
+from flask_restx import Api, Resource, fields, Namespace
 
 app = Flask(__name__)
 model = joblib.load('gender_model/model/logistic_gender.pkl')
@@ -11,40 +11,48 @@ vectorizer = joblib.load('gender_model/model/countvectorizer_gender.pkl')
 app.config.from_object(Config)
 api_path_prefix = app.config['API_PATH_PREFIX']
 api_model_version = app.config['MODEL_VERSION']
+logger = config_Logger(app.logger).config_level()
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('home_page.html')
 
 
-@app.route(f'{api_path_prefix}/health', methods=['GET'])
-def check_health():
-    try:
-        X_pred_vec = vectorizer.transform(['peter'])
-        y_pred = model.predict(X_pred_vec)
-        y_pred = int(y_pred[0])
-        res = {'alive': y_pred}
-        return jsonify(res)
-    except Exception as e:
-        res = {'alive': str(e)}
-        return jsonify(res), 500
+api = Api(
+    app, 
+    version='1.0', 
+    title='Gender Predict',
+    description='Predict the gender base on first name',
+    doc='/api/doc'
+)
+ns = api.namespace('Gender V1', description='Predict the gender base on first name')
 
-@app.route(f'{api_path_prefix}/predict_gender_v1', methods=['POST'])
-def predict_gender():
-    try:
-        json_data = request.get_json()
-        # df = pd.DataFrame(json_data, index=[0])
-        df = pd.DataFrame(json_data)
-        X_pred_vec = vectorizer.transform(df['first_name'].values)
-        y_pred = model.predict(X_pred_vec)
-        predicted_genders = ['M' if pred == 1 else 'F' for pred in y_pred]
-        res = {'Predict of gender': predicted_genders}
-        # app.logger.info(f"predict of {df['first_name'].values} is: {predicted_genders}")
-        return jsonify(res)
-    except Exception as e:
-        # app.logger.error(f"Exception occurred: {str(e)}")
-        res = {'err': e}
-        return jsonify(res), 500
+
+
+input_model = api.model('InputModel', {
+    'first_name': fields.String(description='First name for gender prediction')
+})
+prediction_model = api.model('PredictionModel', {
+    "Predict of gender": fields.List(fields.String(default=''), description='Predicted gender', as_list=True)
+})
+@ns.route(f'{api_path_prefix}/predict_gender_v1', methods=['POST'])
+class PredictGender(Resource):
+    @api.doc(params={'data': 'List of objects with first_name'})
+    @api.expect([input_model], validate=True)
+    @api.marshal_with(prediction_model)
+    def post(self):
+        try:
+            json_data = request.get_json()
+            df = pd.DataFrame(json_data)
+            X_pred_vec = vectorizer.transform(df['first_name'].values)
+            y_pred = model.predict(X_pred_vec)
+            predicted_genders = ['M' if pred == 1 else 'F' for pred in y_pred]
+            logger.info(f"predict gender of {df['first_name'].values} is: {predicted_genders}")
+            res = {'gender': predicted_genders}
+            return res
+        except Exception as e:
+            res = {'error': str(e)}
+            return jsonify(res), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
